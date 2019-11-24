7fad58860d87cedeeba72cbc5c72303d9959f4a9
jihadbou15 <jihad.bou15@outlook.com>
2019-11-24 18:30:43 +0100

    Base Card is done

--- MortalGamblingProj/Assets/Scripts/Card.cs
+++ MortalGamblingProj/Assets/Scripts/Card.cs
@@ -1,18 +1,55 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
 
-public class Card : MonoBehaviour
+public class Card : MonoBehaviour, IPointerClickHandler, IPointerEnterHandler, IPointerExitHandler
 {
-    // Start is called before the first frame update
-    void Start()
+    //Enums
+    public enum Type
     {
-        
+        Melee,
+        Magic
+    }
+    public enum Target
+    {
+        Head,
+        Body,
+        Legs
     }
 
-    // Update is called once per frame
-    void Update()
+    //Variables
+    [SerializeField] private Type _type;
+    [SerializeField] private Target _target;
+    private Image _image;
+
+    //Events
+    public delegate void Activate(Target target);
+    public event Activate onActivate;
+
+    void Initialized()
     {
-        
+        _image = gameObject.GetComponent<Image>();
+    }
+
+    void Tick()
+    {
+
+    }
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        onActivate.Invoke(_target);
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        _image.color = Color.blue;
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        _image.color = Color.clear;
     }
 }
