8e03e7cae372cb435fe252c1578aef65c6b2d0c3
VectorGamez <viktor.blomme@hotmail.com>
2019-12-01 18:04:57 +0100

    started working on Item

--- MortalGamblingProj/Assets/Scripts/Item.cs
+++ MortalGamblingProj/Assets/Scripts/Item.cs
@@ -1,8 +1,49 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
 
-public class Item : MonoBehaviour
+public class Item : MonoBehaviour, IPointerClickHandler, IPointerEnterHandler, IPointerExitHandler
 {
+    [SerializeField] private Action _action = null;
+    [SerializeField] private List<Texture2D> _itemTextures = new List<Texture2D>();
 
+    private Image _image = null;
+    private bool _registeringInput;
+
+    //Events
+    public delegate void Activate(Action action);
+    public event Activate OnActivate;
+
+    public void Initialize(Action action)
+    {
+        _action = action;
+    }
+
+    public void Tick()
+    {
+
+    }
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        if (_registeringInput) OnActivate.Invoke(_action);
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        if (_registeringInput) _image.color = Color.blue;
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        if (_registeringInput) _image.color = Color.red;
+    }
+
+    public void SetRegisteringInput(bool isRegisteringInput)
+    {
+        _registeringInput = isRegisteringInput;
+        _image.color = Color.red;
+    }
 }
